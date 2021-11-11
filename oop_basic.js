f5724e8827eed689271b257c71e5033827a7a1fe
Marek Tammevali <marek.tammevali@khk.ee>
2021-11-11 16:31:22 +0200

    testing examples!

--- oop_basic.js
+++ oop_basic.js
@@ -1,1 +1,14 @@
-console.log("twitch")
+const marek = createNewPerson('marek');
+marek.name;
+marek.greeting();
+
+
+
+function createNewPerson(name) {
+    const obj = {};
+    obj.name = name;
+    obj.greeting = function() {
+        console.log('Hi! I\'m ' + obj.name + '.');
+    };
+    return obj;
+}
