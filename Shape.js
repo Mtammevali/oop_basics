1bf541b23fda41f4cb3d7ba600ea16a911949ac6
Marek Tammevali <marek.tammevali@khk.ee>
2021-11-18 09:24:35 +0200

    OOJS2 creating Shape.js and adding class with construction.

---
+++ Shape.js
@@ -0,0 +1,12 @@
+class Shape{
+    constructor(name, sides, sideLength) {
+        this.name = name;
+        this.sides = sides;
+        this.sideLength = sideLength;
+    }
+    //method
+    calcPerimeter(){
+        this.perimeter = this.sides * this.sideLength;
+        console.log(this.perimeter)
+    }
+}
--- oop_basic.js
+++ oop_basic.js
@@ -1,15 +1,4 @@
-//shape constructor
-function Shape(name, sides, sideLength) {
-    //attributes aka fields
-    this.name = name;
-    this.sides = sides;
-    this.sideLength = sideLength;
-    //functional- methods
-    this.calcPerimeter = function (){
-        this.perimeter = this.sides * this.sideLength ;
-        console.log(this.perimeter)
-    }
-}
+
 // object creation
 let square = new Shape("square",4,5 );
 let triangle = new Shape("triangle", 3, 3);
